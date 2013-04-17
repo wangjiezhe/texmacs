wangjiezhe/texmacs

commit bac28ba3bebfe95f635e7708ad98227e0e4dad13

LaTeX export: ACM styles: adding \affaddr{} on each affiliation line
git-svn-id: http://svn.sv.gnu.org/svn/texmacs/trunk/src@6926 64cb5145-927a-446d-8aed-2fb7b4773692

TeXmacs/progs/convert/latex/tmtex-acm.scm
@@ -85,7 +85,15 @@
 
 (tm-define (tmtex-author-affiliation t)
   (:mode acm-style?)
-  (acm-line-break `(affaddr ,(tmtex (cadr t)))))
+    (with aff-lines
+      (if (list>0? (cadr t))
+        (map (lambda (x)
+                (if (== x '(next-line))
+                  '(!nextline)
+                  `(affaddr ,(tmtex x))))
+             (cdadr t))
+        '())
+    (acm-line-break `(!concat ,@aff-lines))))
 
 (tm-define (tmtex-author-email t)
   (:mode acm-style?)

commit 05e0ed28aa6bbb9d281bae9d236299e520eef53a

LaTeX export: fix bug introduced in r6926
git-svn-id: http://svn.sv.gnu.org/svn/texmacs/trunk/src@6935 64cb5145-927a-446d-8aed-2fb7b4773692

TeXmacs/progs/convert/latex/tmtex-acm.scm
@@ -91,7 +91,7 @@
                   '(!nextline)
                   `(affaddr ,(tmtex x))))
              (cdadr t))
-        '())
+        (if (null? (cdr t)) '() `((affaddr ,(tmtex (cadr t))))))
     (acm-line-break `(!concat ,@aff-lines))))
 
 (tm-define (tmtex-author-email t)