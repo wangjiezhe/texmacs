wangjiezhe/texmacs

commit 412868d57d530beca8699cfe70bd3a3b26b8669a

LaTeX: add support for flalign and alignat
git-svn-id: http://svn.sv.gnu.org/svn/texmacs/trunk/src@7855 64cb5145-927a-446d-8aed-2fb7b4773692

TeXmacs/progs/convert/latex/tmtex.scm
@@ -1012,7 +1012,7 @@
     (apply string-append 
 	   (tmtex-table-args-assemble lb (cons (car lb) rb) l))))
 
-(define (tmtex-table-apply key x)
+(define (tmtex-table-apply key args x)
   (let* ((props (logic-ref tmtex-table-props% key)))
     (if props
 	(let* ((env (if (tmtex-math-mode?) 'array 'tabular))
@@ -1024,14 +1024,14 @@
 	       (e (list '!begin (symbol->string env) (tmtex-table-args p)))
 	       (r (tmtex-table-make p)))
 	  (tex-concat (list before (list e r) after)))
-	(list (list '!begin (symbol->string key))
+	(list `(!begin ,(symbol->string key) ,@args)
 	      (tmtex-table-make (tmtable-parser x))))))
 
 (define (tmtex-tformat l)
-  (tmtex-table-apply 'tabular (cons 'tformat l)))
+  (tmtex-table-apply 'tabular '() (cons 'tformat l)))
 
 (define (tmtex-table l)
-  (tmtex-table-apply 'tabular (cons 'table l)))
+  (tmtex-table-apply 'tabular '() (cons 'table l)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Local and global environment changes
@@ -1715,7 +1715,7 @@
 
 (define (tmtex-eqnarray s l)
   (tmtex-env-set "mode" "math")
-  (let ((r (tmtex-table-apply (string->symbol s) (car l))))
+  (let ((r (tmtex-table-apply '() (string->symbol s) (car l))))
     (tmtex-env-reset "mode")
     r))
 
@@ -1977,22 +1977,25 @@
 
 (define (tmtex-apply key args)
   (let ((n (length args))
-	(r (or (ahash-ref tmtex-dynamic key) (logic-ref tmtex-methods% key))))
+        (r (or (ahash-ref tmtex-dynamic key) (logic-ref tmtex-methods% key))))
     (if (in? key '(quote quasiquote unquote)) (set! r tmtex-noop))
     (cond ((== r 'environment)
            (tmtex-std-env (symbol->string key) args))
           (r (r args))
           (else
             (let ((p (logic-ref tmtex-tmstyle% key)))
-              (if (and p (or (= (cadr p) -1) (= (cadr p) n)))
-                ((car p) (symbol->string key) args)
-                (if (and p (= (cadr p) -2))
-                  ((car p) `(,key ,@args))
-                  (if (and (= n 1)
-                           (or (func? (car args) 'tformat)
-                               (func? (car args) 'table)))
-                    (tmtex-table-apply key (car args))
-                    (tmtex-function key args)))))))))
+              (cond ((and p (or (= (cadr p) -1) (= (cadr p) n)))
+                     ((car p) (symbol->string key) args))
+                    ((and p (= (cadr p) -2)) ((car p) `(,key ,@args)))
+                    ((and (= n 1)
+                          (or (func? (car args) 'tformat)
+                              (func? (car args) 'table)))
+                     (tmtex-table-apply key '() (car args)))
+                    ((and (= n 2)
+                          (or (func? (cAr args) 'tformat)
+                              (func? (cAr args) 'table)))
+                     (tmtex-table-apply key (cDr args) (cAr args)))
+                    (else (tmtex-function key args))))))))
 
 (define (tmtex-function f l)
   (if (== (string-ref (symbol->string f) 0) #\!)

TeXmacs/progs/convert/latex/latex-drd.scm
@@ -230,16 +230,22 @@
   begin-formula begin-equation*
   begin-math begin-displaymath begin-equation
   begin-eqnarray begin-eqnarray*
+  begin-flalign begin-flalign*
   begin-align begin-align*
   begin-multline begin-multline*
   begin-gather begin-gather*
   begin-eqsplit begin-eqsplit*)
+
+(logic-group latex-math-environment-1%
+  begin-alignat begin-alignat*)
 
 (logic-rules
   ((latex-arity% 'x 0) (latex-control% 'x))
   ((latex-arity% 'x 0) (latex-operator% 'x))
   ((latex-environment-0*% 'x) (latex-list% 'x))
   ((latex-math-environment% 'x) (latex-math-environment-0% 'x))
+  ((latex-math-environment% 'x) (latex-math-environment-1% 'x))
+  ((latex-environment-1% 'x) (latex-math-environment-1% 'x))
   ((latex-environment-0% 'x) (latex-math-environment-0% 'x)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;

src/Data/Convert/Tex/fromtex.cpp
@@ -2687,7 +2687,8 @@ finalize_layout (tree t) {
 	}
 
 	insert_return (r);
-	r << tree (BEGIN, translate_list (v[0]->label));
+        v[0]= translate_list (v[0]->label);
+	r << v;
 	spc_flag = true;
 	item_flag= (latex_type ("\\begin-" * v[0]->label) == "list");
 	continue;

src/Data/Convert/Texmacs/upgradetm.cpp
@@ -1374,6 +1374,19 @@ upgrade_split (tree t, bool eq= false) {
         return r;
       }
     }
+    else if (upgrade_tex_flag && (n == 2 || is_func (t, EXPAND, 3))) {
+      string s= as_string (L(t));
+      if (is_func (t, EXPAND, 3) && is_atomic (t[0])) s= t[0]->label;
+      if (ends (s, "*")) s= s (0, N(s)-1);
+      if (s == "alignat") {
+        tree arg= t[n-1];
+        if (is_func (arg, DOCUMENT, 1)) arg= arg[0];
+        if (!is_concat (arg)) arg= tree (CONCAT, arg);
+        r= copy (t);
+        r[n-1]= upgrade_split (arg, true);
+        return r;
+      }
+    }
     for (i=0; i<n; i++)
       r[i]= upgrade_split (t[i]);
     return r;

commit 064c5fbf44066bf75ee9861a42b72e99ac1c3cc1

LaTeX export: fix bug introduced in r7855
git-svn-id: http://svn.sv.gnu.org/svn/texmacs/trunk/src@7866 64cb5145-927a-446d-8aed-2fb7b4773692

TeXmacs/progs/convert/latex/tmtex.scm
@@ -1715,7 +1715,7 @@
 
 (define (tmtex-eqnarray s l)
   (tmtex-env-set "mode" "math")
-  (let ((r (tmtex-table-apply '() (string->symbol s) (car l))))
+  (let ((r (tmtex-table-apply (string->symbol s) '() (car l))))
     (tmtex-env-reset "mode")
     r))
 